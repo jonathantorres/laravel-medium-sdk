dd987286901966a4d9a0e46db431b701867c3282
Jonathan Torres <jonathantorres41@gmail.com>
2017-04-03 22:44:11 -0400

    Add initial service provider and test.

---
+++ src/LaravelMediumSdkServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace JonathanTorres\LaravelMediumSdk;
+
+use Illuminate\Support\ServiceProvider;
+use JonathanTorres\MediumSdk\Medium;
+
+class LaravelMediumSdkServiceProvider extends ServiceProvider
+{
+    /**
+     * Publish configuration file.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/laravel-medium-sdk.php' => config('laravel-medium-sdk.php'),
+        ]);
+    }
+
+    /**
+     * Register MediumSdk in the container.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind('JonathanTorres\LaravelMediumSdk\LaravelMediumSdk', function ($app) {
+            return new Medium([
+                'client-id' => config('laravel-medium-sdk.client-id'),
+                'client-secret' => config('laravel-medium-sdk.client-secret'),
+                'redirect-url' => config('laravel-medium-sdk.redirect-url'),
+                'state' => config('laravel-medium-sdk.state'),
+                'scopes' => config('laravel-medium-sdk.scopes'),
+            ]);
+        });
+    }
+}
---
+++ tests/LaravelMediumSdkServiceProviderTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace JonathanTorres\LaravelMediumSdk\Tests;
+
+use JonathanTorres\LaravelMediumSdk\LaravelMediumSdkServiceProvider;
+use PHPUnit_Framework_TestCase;
+
+class LaravelMediumSdkServiceProviderTest extends PHPUnit_Framework_TestCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
